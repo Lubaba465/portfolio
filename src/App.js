547aa6673965fe69464d8491a44bdccc8e7d1b38
lubaba465 <lubaba.almohammad@uni-oldenburg.de>
2022-08-22 21:26:33 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import React from 'react'
 import Header from './components/Header_footer/header'
 import './resources/styles.css'
-/*import Featured from "../../../PhpstormProjects/portfolioLubaba/src/components/featured";
-import Contact from "../../../PhpstormProjects/portfolioLubaba/src/components/contact";
+import Featured from "./components/featured";
+/*import Contact from "./components/contact";*/
 
-import {Element} from 'react-scroll'*/
+import {Element} from 'react-scroll'
 import AOS from 'aos';
-import {BrowserRouter, BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route,   Switch} from "react-router-dom";
 
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-/*import Skills from "../../../PhpstormProjects/portfolioLubaba/src/components/Skills";
-import Footer from '../../../PhpstormProjects/portfolioLubaba/src/components/Header_footer/footer'
-import Projects from "../../../PhpstormProjects/portfolioLubaba/src/components/Projects";
-import AllProjects from "../../../PhpstormProjects/portfolioLubaba/src/components/Allproject";*/
+import Footer from './components/Header_footer/footer'
+import Projects from "./components/Projects";
+/*
+import AllProjects from "./components/Allproject";
+*/
 AOS.init();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Router>
-                <Header
-              />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Router>
+                    <Header
+                    />
 
-              <main>
+                    <main>
 
-   {/*           <Switch>
-                  <Route exact path="/" component={Featured} />
-                  <Route exact path="/contact" component={Contact} />
-
-
-                  <Route exact path="/portfolio" component={AllProjects} />
-              </Switch>
+                        <Switch>
+                            <Route exact path="/" component={Featured} />
+{/*
+                            <Route exact path="/contact" component={Contact} />
 
 
-              <footer>  <Footer/></footer>
-                  */}
-              </main>
-          </Router>
-          <>
+
+                            <Route exact path="/portfolio" component={AllProjects} />*/}
+                        </Switch>
+                    </main>
+
+                    <footer>  <Footer/></footer>
+
+                </Router>
+                <>
 
 
-          </>
-         </header>
-    </div>
-  );
+                </>
+            </header>
+        </div>
+    );
 }
 
 export default App;
